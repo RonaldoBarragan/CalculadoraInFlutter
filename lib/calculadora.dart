9f13a41992314833fb15c5fdd500c43aab4de5a8
APRENDIZ <barraganronaldo1@gmail.com>
2026-06-04 21:09:20 -0500

    implementacion de switchs cases para las operaciones

--- lib/calculadora.dart
+++ lib/calculadora.dart
@@ -8,30 +8,38 @@ class Calculadora extends StatefulWidget {
   
   }
   class _CalculadoraState extends State<Calculadora>{
+
+
     String operacion = "";
+    String resultado = "0";
+    String numeroActual = "";
+    List<String> elementos = [];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       //appBar: AppBar(title: const Text('Calculadora')),
       backgroundColor: Colors.black,
       body: SafeArea(child: Column(
         children: [
           Expanded(child: Container(
             width: double.infinity,
-            padding: const EdgeInsets.all(20),
+            padding: const EdgeInsets.all(40),
             color: const Color.fromARGB(255, 12, 4, 4),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Text(operacion,
-                style: const TextStyle(color: Colors.white, fontSize: 20),
+                style:  TextStyle(color: Colors.white, fontSize: 20),
                 ),
-                const SizedBox(height: 10,),
-                Text(operacion,
-                style: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                 SizedBox(height: 10,),
+                Text(resultado,
+                style:  TextStyle(color: Colors.white, fontSize: 20),
                 ),
+                
 
               ],
               ),
@@ -40,28 +48,35 @@ class Calculadora extends StatefulWidget {
           Row(
             spacing: 8,
             children: [
-              boton('7'), boton('8'), boton('9')
+              boton('C'), boton('CE'), boton('%'), boton('/')
             ],
           ),
           SizedBox(height: 5,),
           Row(
             spacing: 8,
             children: [
-            boton('4'), boton('5'), boton('6')
+              boton('7'), boton('8'), boton('9'), boton('*')
             ],
           ),
           SizedBox(height: 5,),
           Row(
             spacing: 8,
             children: [
-            boton('1'), boton('2'), boton('3')
+            boton('4'), boton('5'), boton('6'), boton('-')
             ],
           ),
           SizedBox(height: 5,),
           Row(
             spacing: 8,
             children: [
-            boton('Exp'), boton('0'), boton('.')
+            boton('1'), boton('2'), boton('3'), boton('+')
+            ],
+          ),
+          SizedBox(height: 5,),
+          Row(
+            spacing: 8,
+            children: [
+            boton('Exp'), boton('0'), boton('.'), boton('=')
             ],
           )
         ],
@@ -69,10 +84,48 @@ class Calculadora extends StatefulWidget {
       )
     );
   }//build
+
+void actionButon (String valor){
+  //print (valor);
+  setState(() {
+      switch (valor) {
+      case 'C':
+        operacion = '';
+        resultado = '0';
+        numeroActual = '';
+        elementos.clear();
+      break; 
+      case  'CE':
+      if (operacion.isNotEmpty) {
+          operacion = operacion.substring(0, operacion.length - 1);
+        }
+      
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      if(numeroActual.isNotEmpty){
+        elementos.add(numeroActual);
+        elementos.add(valor);
+        numeroActual ="";
+        operacion +=valor;
+      } 
+      print (elementos);
+
+      break;
+
+      default:
+        operacion += valor;
+        numeroActual += valor;
+        }
+  });
+}
+
 Widget boton(String texto) {
   return Expanded(
     child: ElevatedButton(
-      onPressed: (){},
+      onPressed: (){ actionButon(texto);
+      },
       child: Text(texto),
     ),
   );
